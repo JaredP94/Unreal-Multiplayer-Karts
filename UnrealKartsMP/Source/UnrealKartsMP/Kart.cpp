6269788150c4ec10203a7744fad40631cacccd07
Jared <jaredping@yahoo.com>
2019-04-24 21:47:36 +0200

    Replace vehicle with simplified actor + Back and forth velocity

---
+++ UnrealKartsMP/Source/UnrealKartsMP/Kart.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Kart.h"
+
+#include "Components/InputComponent.h"
+
+// Sets default values
+AKart::AKart()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AKart::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AKart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector Translation = Velocity * DeltaTime * 100;
+	AddActorWorldOffset(Translation);
+}
+
+// Called to bind functionality to input
+void AKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AKart::MoveForward);
+}
+
+void AKart::MoveForward(float Value)
+{
+	Velocity = GetActorForwardVector() * Value * 20;
+}
+
